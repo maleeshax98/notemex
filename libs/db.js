7d61d8367f2e9e2b4f76f02927fd61d3ad840eeb
MaleeshaX <142221263+maleeshax98@users.noreply.github.com>
2023-11-21 16:33:52 +0530

    Update db.js

--- libs/db.js
+++ libs/db.js
@@ -1,14 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 
-let prisma;
+global.prisma = global.prisma || new PrismaClient({ log: ["info"] });
 
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
+if (process.env.NODE_ENV !== "production") {
+  global.prisma = prisma;
 }
 
-export default prisma;
+module.exports = global.prisma;
+
